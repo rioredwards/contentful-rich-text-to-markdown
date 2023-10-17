b6a8d14376d7d20e7d9c88430b6da6df7248328b
Rio Edwards <rioredwards@gmail.com>
2023-10-17 13:28:52 -0700

    Update tests to include embedded entry

--- mocks/mocks.ts
+++ mocks/mocks.ts
@@ -77,9 +77,41 @@ export const richTextWithCodeBlock: Document = {
       nodeType: BLOCKS.PARAGRAPH,
       data: {},
       content: [
+        { nodeType: "text", value: "1. Download and cd into the project", marks: [], data: {} },
+      ],
+    },
+    {
+      nodeType: BLOCKS.EMBEDDED_ENTRY,
+      data: {
+        target: {
+          metadata: { tags: [] },
+          sys: {
+            space: { sys: { type: "Link", linkType: "Space", id: "l329ngjcm8m3" } },
+            id: "6IoowmQ4rIHAP4rHezXIvX",
+            type: "Entry",
+            createdAt: "2023-10-15T22:36:59.968Z",
+            updatedAt: "2023-10-16T04:49:29.650Z",
+            environment: { sys: { id: "master", type: "Link", linkType: "Environment" } },
+            revision: 5,
+            contentType: { sys: { type: "Link", linkType: "ContentType", id: "codeBlock" } },
+            locale: "en-US",
+          },
+          fields: {
+            title: "Tic Tac Toe Download",
+            content: "git clone https://github.com/rioredwards/Tic-Tac-Toe.git\\ncd Tic-Tac-Toe",
+            language: "bash",
+          },
+        },
+      },
+      content: [],
+    },
+    {
+      nodeType: BLOCKS.PARAGRAPH,
+      data: {},
+      content: [
         {
           nodeType: "text",
-          value: "1. Download the code and cd into the project directory",
+          value: "2. Compile source code into binary (executable) file",
           marks: [],
           data: {},
         },
@@ -89,14 +121,53 @@ export const richTextWithCodeBlock: Document = {
       nodeType: BLOCKS.EMBEDDED_ENTRY,
       data: {
         target: {
+          metadata: { tags: [] },
           sys: {
-            id: "6IoowmQ4rIHAP4rHezXIvX",
-            type: "Link",
-            linkType: "Entry",
+            space: { sys: { type: "Link", linkType: "Space", id: "l329ngjcm8m3" } },
+            id: "2WVtiGV7KEvIVNDJoEe50d",
+            type: "Entry",
+            createdAt: "2023-10-16T04:44:29.573Z",
+            updatedAt: "2023-10-16T04:44:29.573Z",
+            environment: { sys: { id: "master", type: "Link", linkType: "Environment" } },
+            revision: 1,
+            contentType: { sys: { type: "Link", linkType: "ContentType", id: "codeBlock" } },
+            locale: "en-US",
           },
+          fields: { title: "Tic Tac Toe Compile Into Binary", content: "make", language: "bash" },
         },
       },
       content: [],
+    },
+    {
+      nodeType: BLOCKS.PARAGRAPH,
+      data: {},
+      content: [{ nodeType: "text", value: "3. Run the executable file", marks: [], data: {} }],
+    },
+    {
+      nodeType: BLOCKS.EMBEDDED_ENTRY,
+      data: {
+        target: {
+          metadata: { tags: [] },
+          sys: {
+            space: { sys: { type: "Link", linkType: "Space", id: "l329ngjcm8m3" } },
+            id: "Vv8gPIT5uLr3353UjiGBt",
+            type: "Entry",
+            createdAt: "2023-10-16T04:45:22.572Z",
+            updatedAt: "2023-10-16T04:45:22.572Z",
+            environment: { sys: { id: "master", type: "Link", linkType: "Environment" } },
+            revision: 1,
+            contentType: { sys: { type: "Link", linkType: "ContentType", id: "codeBlock" } },
+            locale: "en-US",
+          },
+          fields: { title: "Tic Tac Toe Run Executable", content: "./main", language: "bash" },
+        },
+      },
+      content: [],
+    },
+    {
+      nodeType: BLOCKS.PARAGRAPH,
+      data: {},
+      content: [{ nodeType: "text", value: "", marks: [], data: {} }],
     },
   ],
 };
